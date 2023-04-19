a8f7631c432131abf81b5d5ee57d9c040d6890b9
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-04-20 02:29:26 +0600

    1

--- ads2/src/MyArrayList.java
+++ ads2/src/MyArrayList.java
@@ -1,11 +1,20 @@
 public class MyArrayList<T> implements myLIst<T> {
-    private static void final int DEFOULT_CAPACITY = 20;
-    private Object[] element;
+    private static void final int INITIAL_CAPACITY = 20;
+    private Object[] elements;
     private int size;
-    public myArrayList(){
-        element = new Object[DEFOULT_CAPACITY];
+    public MyArrayList(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity cannot be negative");
+        }
+        elements = new Object[capacity];
         size = 0;
-
+    }
+    public MyArrayList(int capacity){
+        if (capacity<0){
+            throw new IllegalArgumentException("Capacity cannot be negative");
+        }
+        elements=new Object[[capacity];
+        size = 0;
     }
 
 }
