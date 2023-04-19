8fd717b488d031acdc93c11c646d56c6b8224497
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-04-20 02:36:54 +0600

    1

--- ads2/src/MyArrayList.java
+++ ads2/src/MyArrayList.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.Elements;
+
 public class MyArrayList<T> implements myLIst<T> {
     private static void final int INITIAL_CAPACITY = 20;
     private Object[] elements;
@@ -16,5 +18,24 @@ public class MyArrayList<T> implements myLIst<T> {
         elements=new Object[[capacity];
         size = 0;
     }
-
+    public int size(){
+        return size;
+    }
+    public boolean contains(Object item){
+        for (int i=0; i<size; i++){
+            if (elements[i].equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void add(T item){
+        if (size==elements.length){
+            Object[] newElements=new Object[elements.length*2];
+            System.arraycopy(elements, 0, newElements,0,elements.length);
+            elements=newElements;
+        }
+        elements[size]=item;
+        size++;
+    }
 }
