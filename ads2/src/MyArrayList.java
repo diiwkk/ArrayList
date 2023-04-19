a4d7b4eee42bf84bddf227c2c2daa25bf84bbae2
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-04-20 01:41:29 +0600

    1

--- ads2/src/MyArrayList.java
+++ ads2/src/MyArrayList.java
@@ -1,2 +1,10 @@
 public class MyArrayList {
+    public static void main(String[] args) {
+        myList<String> list= new MyArrayList<>();
+        list.add("111");
+        list.add("222");
+        list.add("333");
+        list.add("444");
+        System.out.println("size:" +list.size());
+    }
 }
