eb58f33caed9c17f7b41279c105cbc5d33b2ab44
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:59:57 +0600

    1

--- ads2/src/LinkedListQueue.java
+++ ads2/src/LinkedListQueue.java
@@ -1,2 +1,22 @@
-public class LinkedListQueue {
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class LinkedListQueue<T> {
+    private LinkedList<T> list;
+
+    public LinkedListQueue() {
+        list = new LinkedList<T>();
+    }
+
+    public void enqueue(T value) {
+        list.addLast(value);
+    }
+
+    public T dequeue() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.removeFirst();
+    }
+
 }
