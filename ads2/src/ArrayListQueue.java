d95905758f40c88d987086bb4f3917bcbe22b111
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:57:56 +0600

    1

--- ads2/src/ArrayListQueue.java
+++ ads2/src/ArrayListQueue.java
@@ -4,26 +4,33 @@ import java.util.NoSuchElementException;
 public class ArrayListQueue<T> {
     private ArrayList<T> list;
 
-    public ArrayListQueue() {
+    public ArrayListQueue(){
         list = new ArrayList<T>();
     }
 
-    public void enqueue(T value) {
+    public void enqueue(T value){
         list.add(value);
     }
 
-    public T dequeue() {
-        if (list.isEmpty()) {
+    public T dequeue(){
+        if(list.isEmpty()){
             throw new NoSuchElementException();
         }
         return list.remove(0);
     }
 
-    public T peek() {
-        if (list.isEmpty()) {
+    public T peek(){
+        if(list.isEmpty()){
             throw new NoSuchElementException();
         }
         return list.get(0);
     }
 
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    public int size(){
+        return list.size();
+    }
 }
