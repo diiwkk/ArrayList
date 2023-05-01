f520ad52642086f1e5e7d93a8487c7041a15d37f
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:42:49 +0600

    1

--- ads2/src/LinkedListStack.java
+++ ads2/src/LinkedListStack.java
@@ -1,2 +1,21 @@
+import java.util.LinkedList;
+import java.util.EmptyStackException;
+
 public class LinkedListStack {
+    private LinkedList<Integer> list;
+
+    public LinkedListStack() {
+        list = new LinkedList<>();
+    }
+
+    public void push(int value) {
+        list.addFirst(value);
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.removeFirst();
+    }
 }
