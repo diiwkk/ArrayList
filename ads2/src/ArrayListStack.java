b0dc7cee29788d107d7001ae2e66b453dd920960
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:40:08 +0600

    1

--- ads2/src/ArrayListStack.java
+++ ads2/src/ArrayListStack.java
@@ -3,13 +3,34 @@ import java.util.EmptyStackException;
 public class ArrayListStack {
     private myArrayList<Integer> list;
 
-    public ArrayListStack() {
+    public ArrayListStack(){
         list = new myArrayList<>();
     }
 
-    public void push(int value) {
+    public void push(int value){
         list.add(value);
     }
 
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.remove(list.size() - 1);
+    }
+
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty(){
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
 
