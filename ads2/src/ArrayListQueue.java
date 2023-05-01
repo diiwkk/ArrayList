fd7b439a3e55093f91257ec77cc78c6dee09f906
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:55:57 +0600

    1

--- ads2/src/ArrayListQueue.java
+++ ads2/src/ArrayListQueue.java
@@ -1,2 +1,15 @@
-public class ArrayListQueue {
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class ArrayListQueue<T> {
+    private ArrayList<T> list;
+
+    public ArrayListQueue() {
+        list = new ArrayList<T>();
+    }
+
+    public void enqueue(T value) {
+        list.add(value);
+    }
+
 }
