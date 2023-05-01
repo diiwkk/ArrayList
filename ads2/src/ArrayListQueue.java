70978a5f4ae5c0ea475314bb930b35f890304053
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-01 23:57:40 +0600

    1

--- ads2/src/ArrayListQueue.java
+++ ads2/src/ArrayListQueue.java
@@ -12,4 +12,18 @@ public class ArrayListQueue<T> {
         list.add(value);
     }
 
+    public T dequeue() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.remove(0);
+    }
+
+    public T peek() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.get(0);
+    }
+
 }
